a4573386ec40e9a67ec6fb6cb063ad0c69ec75d3
ahmed <119942184+bobstoun@users.noreply.github.com>
2023-02-04 11:18:33 +0300

    version 2

--- Program.cs
+++ Program.cs
@@ -1,26 +1,70 @@
+//Console.Clear();
+//int v = Convert.ToInt32(Console.ReadLine());
+//int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+//int elimentobb= 0, elimentoff= 0;
+//foreach(int eliment in array)
+//{
+  //  if(eliment % 2 == 1)
+    //Console.WriteLine($"{eliment}");
+    //elimentoff++;
+//}
+//foreach(int eliment in array)
+//{
+  //  if(eliment % 2 == 0)
+   // Console.WriteLine($"{eliment}");
+    //elimentobb++;
+//}
+//if(elimentoff >= elimentobb)
+//Console.WriteLine("yes");
+//else
+//Console.WriteLine("no");
+
+
+//задайте одномерный массив заполненый случайными числами, найдите сумму элиментов стояших на не четных позициях 
+//Console.Clear();
+//void leo (int[] array)
+//{
+ // for(int i=0; i<array.Length;i++)
+ // array[i] = new Random().Next(-13,99);
+//}
+
+//int kity (int[] array)
+
+//{
+//int NumbersNO= 0;
+  //  for(int i=1;i<array.Length;i +=2)
+   // NumbersNO += array[i];
+   // return NumbersNO ;
+//}
+
+//Console.Write("ведите число ");
+//int v = Convert.ToInt32(Console.ReadLine());
+//int[] array = new int[v];
+//leo(array);
+//Console.WriteLine($"{string.Join(" ", array)}");
+//kity(array);
+//Console.WriteLine(kity(array));
+
+
+// Задача 38 задайте массив вешественных чисел. Найдите разницу между максимальным и минимальным элиментом массива
+
+void LUtick(int[] array)
+{
+  for(int i=0;i<array.Length;i++)
+  array[i] = new Random().Next(-12,123);
+}
+
+
+void  leo(int[] array)
+{
+  
+}
+
+
+Console.Clear();
+Console.Write("ведите число ");
 int v = Convert.ToInt32(Console.ReadLine());
-int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int elimentyes = 0,elimentno = 0;
-foreach(int eliment in array)
-{
-    if(eliment % 2 == 1)
-{
-    Console.WriteLine($"{eliment}");
-    elimentyes++;
-
-}
-
-}
-
-foreach(int eliment in array)
-
-   if(eliment % 2 == 0)
-{   
-   Console.WriteLine($"{eliment}");
-   elimentno++;
-}
-
-if(elimentyes>= elimentno)
-Console.WriteLine("yes");
-else
-Console.WriteLine("no");
+int[] array = new int[v];
+LUtick(array);
+Console.WriteLine($"{string.Join(" ", array)}");
+leo(array);
